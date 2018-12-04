e1e1037d98aa32e9f04e15f3644add1b78038b19
unknown <D:\\Mail>
2018-12-04 16:00:57 +0200

    используем синтаксис декораторов класса (@withprofile)

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -5,7 +5,8 @@ import { withProfile } from 'components/HOC/withProfile';
 import Styles from './styles.m.css';
 import PropTypes from 'prop-types';
 
-class Composer extends Component {
+@withProfile
+export default class Composer extends Component {
     static propTypes = {
         _createPost: PropTypes.func.isRequired,
     };
@@ -74,4 +75,4 @@ class Composer extends Component {
     }
 }
 
-export default withProfile(Composer);
+// export default withProfile(Composer);

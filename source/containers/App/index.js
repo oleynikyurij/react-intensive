544743969f6607b06cec971d75b2ea7bb81a60dd
unknown <D:\\Mail>
2018-12-04 16:30:01 +0200

    замена аватара и имени

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -6,12 +6,12 @@ import Feed from 'components/Feed';
 import { Provider } from 'components/HOC/withProfile';
 
 //Instrument
-import avatar from 'theme/assets/Lisa';
+import avatar from 'theme/assets/cat';
 
 const options = {
     avatar,
-    currentUserFirstName: 'Lisa',
-    currentUserLastName:  'Simpson',
+    currentUserFirstName: 'Кот',
+    currentUserLastName:  'Котофеич',
 };
 
 export default class App extends Component {
